e74d873ff7ceefe37d5698e392a581a61320cb87
CoppoloCode <matthewcoppolo@yahoo.com>
2022-09-03 18:20:38 -0400

    Updated/refactored

--- insertProduct-details.js
+++ insertProduct-details.js
@@ -2,48 +2,58 @@
 /* ------------recieves ID and calls functions to create image and description -----------*/
 
 function setupDetails(productId){
-    setImageProduct(productId);
-    setInfoProduct(productId);
+
+    
+    let productDescriptions;
+
+    setImageElement(productId);
+    productDescriptions = getDescriptionsFromDataBase();
+    setDescription(productDescriptions, productId);
+    
+
+
 }
 
 /* ------------gets the image of a product and stores in local storage -----------*/
 
-function setImageProduct(productId){
-    var productElement = document.getElementById(productId).outerHTML
-    var productElement = productElement.split('onclick="setupDetails(id)"').join('');
-    localStorage.setItem("imageSelected", productElement);
-    
+function setImageElement(productId){
+
+    let productImageElement;
+    productImageElement = document.getElementById(productId).outerHTML;
+    productImageElement = productImageElement.split('onclick="setupDetails(id)"').join('');
+    localStorage.setItem("productImageElement" ,  productImageElement);
+
 }
 
 
 /* ------------gets the description of a product and stores in local storage -----------*/
 
-function setInfoProduct(productId){
-    var productElement = document.getElementById(productId).outerHTML
-    var productElement = productElement.split('"');
-    var productId = productElement[3];
-    localStorage.setItem("productId", productId);
 
-    
-}
+function setDescription(productDescriptions, productId){
 
-function setDescription(productDescriptions){
-    var productId = localStorage.getItem("productId");
-    var productDescriptions = productDescriptions.split("\\n");
+    let productDescription;
+    productDescriptions = productDescriptions.split("\\n");
     productDescriptions.length = productDescriptions.length - 1;
-    productDescription = productDescriptions[productId-1];
-    localStorage.setItem("productDescription", productDescription);
+    productDescription = productDescriptions[productId];
+    productDescription = '<p>' + productDescription + '</p>';
+    localStorage.setItem("productDescription" ,  productDescription);
     
 
 }
 
-
-
-/*-------- setting product image and Info on Product-details.html ---------*/
-
-function onLoad(){
-
-    document.getElementById("showImage").innerHTML = localStorage.getItem("imageSelected");
-    document.getElementById("showInfo").innerHTML = '<p>' + localStorage.getItem("productDescription") + '</P>';
-
+function setImageandDescription(){
+    document.getElementById("showImage").innerHTML = localStorage.getItem("productImageElement");
+    document.getElementById("showInfo").innerHTML = localStorage.getItem("productDescription");
 }
+
+function getDescriptionsFromDataBase(){
+    $.ajax({
+        url: 'getDescription.php',
+        type: 'POST',
+        success: function(data){                   
+            let descriptions = data;
+            return descriptions;  
+        }
+    });
+}
+        
--- insertProduct.js
+++ insertProduct.js
@@ -1,39 +1,10 @@
 
 
-/*--------setting the Review for products --------------------*/
+/*--------------------splits products string for formating ---------------*/
 
-function setReview(productReview){
-    productReview = parseInt(productReview);
-    var starElement = '<i class="fa fa-star" aria-hidden="true"></i>';
-    var emptyStarElement = '<i class="fa fa-star-o" aria-hidden="true"></i>';
-    var result = "";
-    for(i=0; i<productReview; i++){
-       result += starElement;
-    }
-    for(i=productReview; i<5; i++){
-        result += emptyStarElement;
-    }
-    return result;
-}
-/*-------------setting the products in their place --------------------*/
+function getFormatedProducts(productsToSplit){
 
-
-function createProduct(productPlacementID, products, chosenProduct){
-
-    var productID = products[chosenProduct][0];
-    var productName = products[chosenProduct][1];
-    var productImage = products[chosenProduct][2];
-    var productReview = products[chosenProduct][3];
-    var productCost = products[chosenProduct][4];
-
-    var review = setReview(productReview);
-    document.getElementById(productPlacementID).innerHTML = "<a href='product-details.html'>" + "<img src='images/" 
-    + productImage + "'" +" "+ "id=" + "'" + productID + "'" + " " + "onclick='setupDetails(id)'></a><h4>" 
-    + productName + "</h4><div>" + review + "</div><p>" + productCost + "</p>";
-}
-
-function splitProducts(productsToSplit){
-    var products = [];     
+    let products = [];     
     products = productsToSplit.split('\\n');
     products[0] = products[0].split("\r\n").join('');
     products.length = products.length - 1;
@@ -46,37 +17,83 @@ function splitProducts(productsToSplit){
     for(i = 0; i<products.length;i++){
         products[i] = products[i].split('-');
     }
-    setProductsAtLocation(products);
+    return products;
 
 }
 
 /*--------------------setting the products in their place ---------------*/
 
-function setProductsAtLocation(products){
+function getRandomProductsId(products){
 
-    var randomProducts = [];
+    let randomProductsId = [];
+    let randomProducts = [];
 
-    var resultToReturn = false;
+    let isUnique = false;
 
     do{
         for(i=0;i<products.length;i++){
-            randomProducts[i] = Math.floor(Math.random() * (products.length));
+            randomProductsId[i] = Math.floor(Math.random() * (products.length));
         }
-        resultToReturn = randomProducts.some((element, index) => {
-            return randomProducts.indexOf(element) !== index
+        isUnique = randomProductsId.some((element, index) => {
+            return randomProductsId.indexOf(element) !== index
         });
-    }while(resultToReturn == true);
+    }while(isUnique == true);
+    for(let i = 0; i < randomProductsId.length; i++){
+        randomProducts[i] = products[randomProductsId[i]];
+    }
     
-    createProduct("product-1", products, randomProducts[0]);
-    createProduct("product-2", products, randomProducts[1]);
-    createProduct("product-3", products, randomProducts[2]);
-    createProduct("product-4", products, randomProducts[3]);
-    createProduct("product-5", products, randomProducts[4]);
-    createProduct("product-6", products, randomProducts[5]);
-    createProduct("product-7", products, randomProducts[6]);
-    createProduct("product-8", products, randomProducts[7]);
+    
+    return randomProducts;
 
 }
+
+/*-------------creates the product Element--------------------*/
+
+function getProductElements(randomProducts){
+
+    let productID;
+    let productName;
+    let productImage;
+    let productReview;
+    let productCost;
+    let review = getReview(productReview);
+    let productElements = [];
+
+    for(let i = 0; i < randomProducts.length; i++){
+
+        productID = randomProducts[i][0];
+        productName = randomProducts[i][1];
+        productImage = randomProducts[i][2];
+        productReview = randomProducts[i][3];
+        productCost = randomProducts[i][4];
+        review = getReview(productReview);
+
+        productElements[i] = "<a href='product-details.html'>" + "<img src='images/" 
+        + productImage + "'" +" "+ "id=" + "'" + productID + "'" + " " + "onclick='setupDetails(id)'></a><h4>" 
+        + productName + "</h4><div>" + review + "</div><p>" + productCost + "</p>";
+    }
+
+    return productElements;
+}
+
+/*--------creates Review Element of products --------------------*/
+
+function getReview(productReview){
+    productReview = parseInt(productReview);
+    let starElement = '<i class="fa fa-star" aria-hidden="true"></i>';
+    let emptyStarElement = '<i class="fa fa-star-o" aria-hidden="true"></i>';
+    let result = "";
+    for(i=0; i<productReview; i++){
+       result += starElement;
+    }
+    for(i=productReview; i<5; i++){
+        result += emptyStarElement;
+    }
+    return result;
+}
+
+
+
 
 
     
