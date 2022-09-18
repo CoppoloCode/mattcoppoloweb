0a04c529879f7428d8fb6c3bd5c0edf2c552e93f
CoppoloCode <matthewcoppolo@yahoo.com>
2022-09-18 18:40:45 -0400

    updated

--- account.js
+++ account.js
@@ -23,10 +23,19 @@ function getAccountData(){
         
         url: "account.php",
         type: "POST",
-        data: {userId: userId},
+        data: {getUserId: userId},
         dataType: "json",
         success: function(data){
             account.accountInfo = (JSON.stringify((data)));
+            account.accountInfo = account.accountInfo.split(",");
+            for(i = 0; i < account.accountInfo.length; i++){
+                account.accountInfo[i] = account.accountInfo[i].split(":");
+            }
+            for(i = 0; i < account.accountInfo.length; i++){
+                account.accountInfo[i] = account.accountInfo[i][1].replaceAll('"',"");
+            }
+            account.accountInfo[5] = account.accountInfo[5].replaceAll("}","");
+            account.accountInfo = account.accountInfo;
             setupAccountInfo();
         },
         error: function(err){
@@ -46,16 +55,9 @@ function signOut(){
 
 function setupAccountInfo(){
 
-    let accountInfo = account.accountInfo.split(",");
+    let accountInfo = account.accountInfo;
 
-    for(i = 0; i < accountInfo.length; i++){
-        accountInfo[i] = accountInfo[i].split(":");
-    }
-    for(i = 0; i < accountInfo.length; i++){
-        accountInfo[i] = accountInfo[i][1].replaceAll('"',"");
-    }
-    accountInfo[5] = accountInfo[5].replaceAll("}","");
-    account.accountInfo = accountInfo;
+    
  
 
     let accountElement = ` <div class="account-title">
@@ -77,6 +79,7 @@ function accountModify(){
     let accountInfo = account.accountInfo;
 
     let accountModifyElement = `<div class="account-title">
+                                    <button onclick="setupAccountInfo()">Go Back</button>
                                     <h1>Your Account Info</h1>
                                 </div>
                                 <div class="accountModify">
@@ -90,6 +93,11 @@ function accountModify(){
                                             <h2>password:</h2>
                                             <p id = 'password'>`+accountInfo[2]+`</p>
                                             <button onclick="changePassword()">Change</button>
+                                        </div>
+                                        <div class="address"> 
+                                            <h2>address:</h2>
+                                            <p id = 'address'>`+accountInfo[3]+`</p>
+                                            <button onclick="changeAddress()">Change</button>
                                         </div>
                                         <div class="firstName">
                                             <h2>First Name:</h2>
@@ -118,19 +126,20 @@ function changeEmail(){
 function updateEmail(email){
 
     account.accountInfo[1] = email;
+    userId = account.accountInfo[0];
 
     $.ajax({
         url: "account.php",
         type: "POST",
-        data: {changeEmail: email},
-        success: function(data){
-            
+        data: {changeEmail: email, user_Id: userId},
+        success: function(){
+            document.getElementsByClassName("email")[0].innerHTML = `<h2>Email:</h2>
+                                                                  <p>`+account.accountInfo[1]+`</p>
+                                                                  <button onclick="changeEmail()">Change</button>`;
         },
         error: function(err){
             console.log(err.responseText);
         }
-
-
 
     })
 
@@ -148,16 +157,35 @@ $(document).on("change","#passInput1, #passInput2",function(){
         document.getElementsByClassName("password")[0].innerHTML = `<h2>Password:</h2>
                                                                 <input id="passInput1" type="password" value="`+pass1+`"></input>
                                                                 <input id="passInput2" type="password" value="`+pass2+`"></input>
-                                                                <button onClick="updatePassword()">Save</button>`
+                                                                <button onClick="updatePassword(document.getElementById('passInput1').value)">Save</button>`
         document.getElementsByClassName("password")[0].outerHTML += `<div class="noMatch"><small>Passwords do not match</small></div>`;
     }else{
         document.getElementsByClassName("password")[0].innerHTML = `<h2>Password:</h2>
                                                                 <input id="passInput1" type="password" value="`+pass1+`"></input>
                                                                 <input id="passInput2" type="password" value="`+pass2+`"></input>
-                                                                <button onClick="updatePassword()">Save</button>`;
+                                                                <button onClick="updatePassword(document.getElementById('passInput1').value)">Save</button>`;
         document.getElementsByClassName("noMatch")[0].innerHTML = '';
     }
+    if((pass1 == '') || (pass2 == '')){
 
+        if(document.getElementsByClassName("blank")[0] != null){
+            document.getElementsByClassName("blank")[0].innerHTML = '';
+        }
+        document.getElementsByClassName("password")[0].innerHTML = `<h2>Password:</h2>
+                                                                <input id="passInput1" type="password" value="`+pass1+`"></input>
+                                                                <input id="passInput2" type="password" value="`+pass2+`"></input>
+                                                                <button onClick="updatePassword(document.getElementById('passInput1').value)">Save</button>`
+        document.getElementsByClassName("password")[0].outerHTML += `<div class="blank"><small>Password cannot be blank</small></div>`;
+
+    }else{
+        document.getElementsByClassName("password")[0].innerHTML = `<h2>Password:</h2>
+                                                                <input id="passInput1" type="password" value="`+pass1+`"></input>
+                                                                <input id="passInput2" type="password" value="`+pass2+`"></input>
+                                                                <button onClick="updatePassword(document.getElementById('passInput1').value)">Save</button>`;
+        if(document.getElementsByClassName("blank")[0] != null){
+            document.getElementsByClassName("blank")[0].innerHTML = '';
+        }                                                        
+    }
 })
 
 function changePassword(){
@@ -165,35 +193,116 @@ function changePassword(){
     document.getElementsByClassName("password")[0].innerHTML = `<h2>Password:</h2>
                                                                 <input id="passInput1" type="password" value=""></input>
                                                                 <input id="passInput2" type="password" value=""></input>
-                                                                <button onClick="updatePassword()">Save</button>`;
+                                                                <button onclick="updatePassword(document.getElementById('passInput1').value)">Save</button>`;
 }
 
-function updatePassword(){
+function updatePassword(password){                            
 
+    account.accountInfo[2] = password;
+    userId = account.accountInfo[0];
+
+    $.ajax({
+        url: "account.php",
+        type: "POST",
+        data: {changePassword: password, user_Id: userId},
+        success: function(){
+            document.getElementsByClassName("password")[0].innerHTML = `<h2>Password:</h2>
+                                                                  <p id='password'>`+account.accountInfo[2]+`</p>
+                                                                  <button onclick="changePassword()">Change</button>`;
+        },
+        error: function(err){
+            console.log(err.responseText);
+        }
+
+    })
     
 
 }
 
+function changeAddress(){
+
+    document.getElementsByClassName("address")[0].innerHTML = `<h2>Address:</h2>
+                                                                <input id="addressInput" type="text" value="`+account.accountInfo[3]+`"></input>
+                                                                <button onclick="updateAddress(document.getElementById('addressInput').value)">Save</button>`;
+}
+
+function updateAddress(address){
+
+    account.accountInfo[3] = address;
+    userId = account.accountInfo[0];
+
+    $.ajax({
+        url: "account.php",
+        type: "POST",
+        data: {changeAddress: address, user_Id: userId},
+        success: function(){
+            document.getElementsByClassName("address")[0].innerHTML = `<h2>Address:</h2>
+                                                                    <p>`+account.accountInfo[3]+`</p>
+                                                                    <button onclick="changeAddress()">Change</button>`;
+        },
+        error: function(err){
+            console.log(err.responseText);
+        }
+
+    })
+
+}
+
+
 function changeFirstName(){
 
     document.getElementsByClassName("firstName")[0].innerHTML = `<h2>First Name:</h2>
-                                                                <input type="text" value="`+account.accountInfo[4]+`"></input>
-                                                                <button onclick="changeFirstName()">Save</button>`;
+                                                                <input id='firstNameInput' type="text" value="`+account.accountInfo[4]+`"></input>
+                                                                <button onclick="updateFirstName(document.getElementById('firstNameInput').value)">Save</button>`;
 }
 
-function updateFirstName(){
+function updateFirstName(firstName){
 
+    account.accountInfo[4] = firstName;
+    userId = account.accountInfo[0];
+
+    $.ajax({
+        url: "account.php",
+        type: "POST",
+        data: {changeFirstName: firstName, user_Id: userId},
+        success: function(){
+            document.getElementsByClassName("firstName")[0].innerHTML = `<h2>First Name:</h2>
+                                                                    <p>`+account.accountInfo[4]+`</p>
+                                                                    <button onclick="changeFirstName()">Change</button>`;
+        },
+        error: function(err){
+            console.log(err.responseText);
+        }
+
+    })
 }
 
 function changeLastName(){
 
     document.getElementsByClassName("lastName")[0].innerHTML = `<h2>Last Name:</h2>
-                                                                <input type="text" value="`+account.accountInfo[5]+`"></input>
-                                                                <button onclick="changeFirstName()">Save</button>`;
+                                                                <input id ='lastNameInput' type="text" value="`+account.accountInfo[5]+`"></input>
+                                                                <button onclick="updateLastName(document.getElementById('lastNameInput').value)">Save</button>`;
 
 }
 
-function updateLastName(){
+function updateLastName(lastName){
 
+    account.accountInfo[5] = lastName;
+    userId = account.accountInfo[0];
+
+    $.ajax({
+        url: "account.php",
+        type: "POST",
+        data: {changeLastName: lastName, user_Id: userId},
+        success: function(){
+            document.getElementsByClassName("lastName")[0].innerHTML = `<h2>Last Name:</h2>
+                                                                    <p>`+account.accountInfo[5]+`</p>
+                                                                    <button onclick="changeLastName()">Change</button>`;
+        },
+        error: function(err){
+            console.log(err.responseText);
+        }
+
+    })
 
 }
--- account.php
+++ account.php
@@ -7,9 +7,9 @@
         die("Connection Failed: " . $conn->connect_error);
     }
     
-    if(isset($_POST['userId'])){
+    if(isset($_POST['getUserId'])){
 
-        $userId = $_POST['userId'];
+        $userId = $_POST['getUserId'];
 
         $sql = "SELECT * FROM accounts WHERE user_id = $userId";
         
@@ -21,6 +21,70 @@
         echo json_encode($result);
 
     }
+
+    if(isset($_POST['changeEmail'])){
+
+        $email = $_POST['changeEmail'];
+        $userId = $_POST['user_Id'];
+
+        $sql = "UPDATE accounts SET email = '$email' WHERE user_id = $userId";
+
+        $result = $conn->query($sql);
+
+        echo $result;
+
+    }
+
+    if(isset($_POST['changePassword'])){
+
+        $password = $_POST['changePassword'];
+        $userId = $_POST['user_Id'];
+
+        $sql = "UPDATE accounts SET password = '$password' WHERE user_id = $userId";
+
+        $result = $conn->query($sql);
+
+        echo $result;
+
+    }
+    if(isset($_POST['changeAddress'])){
+
+        $address = $_POST['changeAddress'];
+        $userId = $_POST['user_Id'];
+
+        $sql = "UPDATE accounts SET address = '$address' WHERE user_id = $userId";
+
+        $result = $conn->query($sql);
+
+        echo $result;
+
+    }
+    if(isset($_POST['changeFirstName'])){
+
+        $firstName = $_POST['changeFirstName'];
+        $userId = $_POST['user_Id'];
+
+        $sql = "UPDATE accounts SET first_name = '$firstName' WHERE user_id = $userId";
+
+        $result = $conn->query($sql);
+
+        echo $result;
+
+    }
+    if(isset($_POST['changeLastName'])){
+
+        $lastName = $_POST['changeLastName'];
+        $userId = $_POST['user_Id'];
+
+        $sql = "UPDATE accounts SET last_name = '$lastName' WHERE user_id = $userId";
+
+        $result = $conn->query($sql);
+
+        echo $result;
+
+    }
+
+
 
 
     $conn->close();
