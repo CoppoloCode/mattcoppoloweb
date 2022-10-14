c0ebb432ffead296fe04ecf1154f61f62fc3729a
CoppoloCode <matthewcoppolo@yahoo.com>
2022-10-14 19:47:40 -0400

    updated

--- setupStoreHome.js
+++ setupStoreHome.js
@@ -23,3 +23,20 @@ function setProducts(data){
         document.getElementById(productPlacementId + i).innerHTML = productElements[i];
     }                 
 }
+
+function revealCategories(){
+    let reveals = document.querySelectorAll(".reveal");
+
+    for(let i = 0; i < reveals.length; i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 150;
+
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add("active");
+        }else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", revealCategories);
