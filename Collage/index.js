2b82a6c1b431dfbde1a6ed0280c646af67ac2d6b
CoppoloCode <matthewcoppolo@yahoo.com>
2022-11-10 16:59:24 -0500

    updated

--- Collage/index.js
+++ Collage/index.js
@@ -1,3 +1,5 @@
+
+// Cards
 const subtitle1 = document.getElementsByClassName("card-subtitle")[0];
 const subtitle2= document.getElementsByClassName("card-subtitle")[1];
 const subtitle3 = document.getElementsByClassName("card-subtitle")[2];
@@ -29,14 +31,40 @@ createSubtitle3("Lorem ipsum dolor sit amet consectetur adipisicing elit.");
 
 
 
-
+// Cards-2
 document.getElementById("cards-2").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card-2")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
+  for(const card of document.getElementsByClassName("card-2")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
+
+
+// Magic Text
+let index = 0,
+    interval = 1000;
+
+const rand = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const animate = star => {
+  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+}
+
+for(const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
+    
+    setInterval(() => animate(star), 1000);
+  }, index++ * (interval / 3))
+}
+
